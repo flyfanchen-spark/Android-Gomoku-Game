682091e494ee330f143f9a549ff7324067b4f207
flyfanchen-spark <flyfan.chen@gmail.com>
2026-03-27 14:15:26 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Gomoku Mobile Game (Android) 五子连珠小游戏 (Android)
+This is my first self-taught Android project. 这是我的第一个自学 Android 作品。
+
+## Features 功能
+-4 difficulty levels with different colors 4 色难度挑战
+-High score record tracking 历史最高分记录
+-Customizable background color 自定义背景颜色
+-Smooth movement animation 丝滑的移动轨迹动画
+
+## How to Run 如何运行
+Download the APK from the Releases page and install it, or clone this project and build it in Android Studio.
+请在 Releases 页面下载 APK 安装，或克隆本项目到 Android Studio 编译。
---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.flyfanchen.fivechess"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.flyfanchen.fivechess"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.3"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
